ffabe446dbc2bd7138b4de5b4cc9b6797307ecb9
SheinMinnOo <sheinmin.oo@kbzbank.com>
2024-04-28 18:53:11 +0700

    first commit

---
+++ lib/database/index.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = "mongodb+srv://sheinminnoojdev:2004142@cluster0.eovxrza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
+
+  cached.conn = await cached.promise;
+  console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
+  return cached.conn;
+}
